0c43923ef20b9f25f561b4cc83877a742736e80e
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2024-01-16 22:34:22 +0100

    Inizio. Visualizzazione di tutte le tabelle

--- SpeedrunningDatabase/Form1.Designer.cs
+++ SpeedrunningDatabase/Form1.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.SpeedrunnersTable = new System.Windows.Forms.DataGridView();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.VideogamesTable = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.SpeedrunsTable = new System.Windows.Forms.DataGridView();
+            this.SpeedrunnersButton = new System.Windows.Forms.Button();
+            this.VideogamesButton = new System.Windows.Forms.Button();
+            this.SpeedrunsButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedrunnersTable)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VideogamesTable)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedrunsTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 426);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.SpeedrunnersButton);
+            this.tabPage1.Controls.Add(this.SpeedrunnersTable);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 400);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Speedrunners";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // SpeedrunnersTable
+            // 
+            this.SpeedrunnersTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SpeedrunnersTable.Location = new System.Drawing.Point(205, 17);
+            this.SpeedrunnersTable.Name = "SpeedrunnersTable";
+            this.SpeedrunnersTable.Size = new System.Drawing.Size(534, 340);
+            this.SpeedrunnersTable.TabIndex = 0;
+            this.SpeedrunnersTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.VideogamesButton);
+            this.tabPage2.Controls.Add(this.VideogamesTable);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 400);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Videogames";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // VideogamesTable
+            // 
+            this.VideogamesTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.VideogamesTable.Location = new System.Drawing.Point(271, 6);
+            this.VideogamesTable.Name = "VideogamesTable";
+            this.VideogamesTable.Size = new System.Drawing.Size(494, 343);
+            this.VideogamesTable.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.SpeedrunsButton);
+            this.tabPage3.Controls.Add(this.SpeedrunsTable);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Speedruns";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // SpeedrunsTable
+            // 
+            this.SpeedrunsTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SpeedrunsTable.Location = new System.Drawing.Point(295, 6);
+            this.SpeedrunsTable.Name = "SpeedrunsTable";
+            this.SpeedrunsTable.Size = new System.Drawing.Size(467, 354);
+            this.SpeedrunsTable.TabIndex = 0;
+            // 
+            // SpeedrunnersButton
+            // 
+            this.SpeedrunnersButton.Location = new System.Drawing.Point(427, 363);
+            this.SpeedrunnersButton.Name = "SpeedrunnersButton";
+            this.SpeedrunnersButton.Size = new System.Drawing.Size(75, 23);
+            this.SpeedrunnersButton.TabIndex = 1;
+            this.SpeedrunnersButton.Text = "Load data";
+            this.SpeedrunnersButton.UseVisualStyleBackColor = true;
+            this.SpeedrunnersButton.Click += new System.EventHandler(this.SpeedrunnersButton_Click);
+            // 
+            // VideogamesButton
+            // 
+            this.VideogamesButton.Location = new System.Drawing.Point(488, 371);
+            this.VideogamesButton.Name = "VideogamesButton";
+            this.VideogamesButton.Size = new System.Drawing.Size(75, 23);
+            this.VideogamesButton.TabIndex = 1;
+            this.VideogamesButton.Text = "Load data";
+            this.VideogamesButton.UseVisualStyleBackColor = true;
+            this.VideogamesButton.Click += new System.EventHandler(this.VideogamesButton_Click);
+            // 
+            // SpeedrunsButton
+            // 
+            this.SpeedrunsButton.Location = new System.Drawing.Point(503, 366);
+            this.SpeedrunsButton.Name = "SpeedrunsButton";
+            this.SpeedrunsButton.Size = new System.Drawing.Size(75, 23);
+            this.SpeedrunsButton.TabIndex = 1;
+            this.SpeedrunsButton.Text = "Load data";
+            this.SpeedrunsButton.UseVisualStyleBackColor = true;
+            this.SpeedrunsButton.Click += new System.EventHandler(this.SpeedrunsButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedrunnersTable)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.VideogamesTable)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.SpeedrunsTable)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView SpeedrunnersTable;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView VideogamesTable;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView SpeedrunsTable;
+        private System.Windows.Forms.Button SpeedrunnersButton;
+        private System.Windows.Forms.Button VideogamesButton;
+        private System.Windows.Forms.Button SpeedrunsButton;
     }
 }
 
--- SpeedrunningDatabase/Form1.cs
+++ SpeedrunningDatabase/Form1.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SpeedrunningDatabase
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+     
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*string connectionString = "server=localhost;uid=programma;pwd=12345;database=speedrunning";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string SRQuery = "SELECT * FROM speedrunner";
+            string VGQuery = "SELECT * FROM videogame";
+            string SRUNQuery = "SELECT * FROM speedrun";
+            connection.Open();
+
+            MySqlCommand com =new MySqlCommand(SRQuery,connection);
+            com.ExecuteNonQuery();
+            MySqlDataAdapter adapt = new MySqlDataAdapter(com);
+            DataTable data = new DataTable();
+            adapt.Fill(data);
+            SpeedrunnersTable.DataSource = data;
+
+            com = new MySqlCommand(VGQuery);
+            com.ExecuteNonQuery();
+            adapt = new MySqlDataAdapter(com);
+            data = new DataTable();
+            adapt.Fill(data);
+            VideogamesTable.DataSource = data;
+
+            com = new MySqlCommand(SRUNQuery);
+            com.ExecuteNonQuery();
+            adapt = new MySqlDataAdapter(com);
+            data = new DataTable();
+            adapt.Fill(data);
+            SpeedrunsTable.DataSource = data;
+            connection.Close();*/
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SpeedrunnersButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;uid=programma;pwd=12345;database=speedrunning";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string SRQuery = "SELECT * FROM speedrunner";
+            connection.Open();
+
+            MySqlCommand com = new MySqlCommand(SRQuery, connection);
+            com.ExecuteNonQuery();
+            MySqlDataAdapter adapt = new MySqlDataAdapter(com);
+            DataTable data = new DataTable();
+            adapt.Fill(data);
+            SpeedrunnersTable.DataSource = data;
+            connection.Close();
+        }
+
+        private void VideogamesButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;uid=programma;pwd=12345;database=speedrunning";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string VGQuery = "SELECT * FROM videogame";
+            connection.Open();
+
+            MySqlCommand com = new MySqlCommand(VGQuery, connection);
+            com.ExecuteNonQuery();
+            MySqlDataAdapter adapt = new MySqlDataAdapter(com);
+            DataTable data = new DataTable();
+            adapt.Fill(data);
+            VideogamesTable.DataSource = data;
+            connection.Close();
+        }
+
+        private void SpeedrunsButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = "server=localhost;uid=programma;pwd=12345;database=speedrunning";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string SRUNQuery = "SELECT * FROM speedrun";
+            connection.Open();
+
+            MySqlCommand com = new MySqlCommand(SRUNQuery, connection);
+            com.ExecuteNonQuery();
+            MySqlDataAdapter adapt = new MySqlDataAdapter(com);
+            DataTable data = new DataTable();
+            adapt.Fill(data);
+            SpeedrunsTable.DataSource = data;
+            connection.Close();
         }
     }
 }
